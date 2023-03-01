4695ec5de93cea167c9c0b87b2e6b330c6d392b4
Fortune <fortune.onyema22@gmail.com>
2023-03-01 14:16:03 +0100

    Flutter project setup

---
+++ android/app/src/main/kotlin/com/example/riverpod_coursecode3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riverpod_coursecode3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: unnecessary_null_comparison
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        title: 'Flutter Riverpod Course 2',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        themeMode: ThemeMode.dark,
+        darkTheme: ThemeData.dark(),
+        home: const HomePage(),
+      ),
+    ),
+  );
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: const Text('Weather')),
+    );
+  }
+}
